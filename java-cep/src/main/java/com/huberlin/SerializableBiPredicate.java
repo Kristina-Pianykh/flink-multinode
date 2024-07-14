223911cb8a5dc6b9dbfb98c7c02f1128c3022d14
Kristina Pianykh <kristinavrnrus@gmail.com>
2024-07-14 16:05:46 +0200

    refactor: modularize maven proj with shared module event

--- java-cep/src/main/java/com/huberlin/SerializableBiPredicate.java
+++
@@ -1,7 +0,0 @@
-package com.huberlin;
-
-import java.io.Serializable;
-import java.util.function.BiPredicate;
-
-@FunctionalInterface
-public interface SerializableBiPredicate<T, U> extends BiPredicate<T, U>, Serializable {}
---
+++ mama-module/java-cep/src/main/java/com/huberlin/SerializableBiPredicate.java
@@ -0,0 +1,7 @@
+package com.huberlin.javacep;
+
+import java.io.Serializable;
+import java.util.function.BiPredicate;
+
+@FunctionalInterface
+public interface SerializableBiPredicate<T, U> extends BiPredicate<T, U>, Serializable {}
