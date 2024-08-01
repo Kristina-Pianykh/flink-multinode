5c511f63df0619632d3b998a80e4962940f0e1ea
Kristina Pianykh <kristinavrnrus@gmail.com>
2024-08-02 00:01:35 +0200

    chore: improve logging for coordinator

--- mama-module/coordinator/src/main/java/coordinator/Coordinator.java
+++ mama-module/coordinator/src/main/java/coordinator/Coordinator.java
@@ -4,18 +4,23 @@ import com.huberlin.event.ControlEvent;
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Coordinator {
+  private static final Logger LOG = LoggerFactory.getLogger(Coordinator.class);
+
   public static void main(String[] args) {
     int port = 6668;
 
     try (ServerSocket serverSocket = new ServerSocket(port)) {
-      System.out.println("Coordinator started. Listening for connections on port " + port + "...");
+      LOG.info("Coordinator started. Listening for connections on port " + port + "...");
       while (true) {
         Socket socket = serverSocket.accept();
         new ClientHandler(socket).start();
       }
     } catch (IOException e) {
+      LOG.error("Failed to start coordinator with error: {}", e.getMessage());
       e.printStackTrace();
       System.exit(-1);
     }
@@ -39,23 +44,23 @@ public class Coordinator {
           String message = input.readLine();
 
           if (message == null || message.contains("end-of-the-stream")) {
-            System.out.println("Reached the end of the stream for " + client_address + "\n");
-            if (message == null)
-              System.out.println("Stream terminated without end-of-the-stream marker.");
+            LOG.info("Reached the end of the stream for " + client_address + "\n");
+            if (message == null) LOG.info("Stream terminated without end-of-the-stream marker.");
             input.close();
             socket.close();
             return;
           } else if (message.startsWith("control")) {
             Optional<ControlEvent> controlEvent = ControlEvent.parse(message);
             assert controlEvent.isPresent() : "Parsing control event " + message + "failed";
-            System.out.println("Received control event: " + controlEvent.get().toString());
+            LOG.info("Received control event: " + controlEvent.get().toString());
           }
         }
       } catch (IOException e) {
-        System.out.println("Client disconnected");
+        LOG.error("Client disconnected with error {}", e.getMessage());
         try {
           socket.close();
-        } catch (IOException ignored) {
+        } catch (IOException exc) {
+          LOG.error("Failed to close socket with error {}", exc.getMessage());
         }
       }
     }
