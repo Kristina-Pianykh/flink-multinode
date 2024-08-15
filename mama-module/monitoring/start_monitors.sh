156c5f71d8e7ce53209677317a0d46c50155027f
Kristina Pianykh <kristinavrnrus@gmail.com>
2024-08-16 01:45:01 +0200

    refactor: use log4j2 file appender for recording rates

--- mama-module/monitoring/start_monitors.sh
+++ mama-module/monitoring/start_monitors.sh
@@ -8,10 +8,15 @@ fi
 echo "INEQUALITY_INPUTS_PATH = $INEQUALITY_INPUTS_PATH"
 
 rm -f monitor*.log
-rm -f totalRates*.log
+rm -f totalRates*.csv
 for i in {0..4}
 do
-  # if [ $i -ne 1 ] ; then
-		java -ea --add-opens java.base/java.lang=ALL-UNNAMED -jar target/monitoring-0.1.jar -node $i -monitoringinputs $INEQUALITY_INPUTS_PATH > monitor$i.log 2>&1 &
-  # fi
+  echo "Starting monitor $i"
+		java -ea \
+      --add-opens java.base/java.lang=ALL-UNNAMED \
+      -Dlog4j.configurationFile=log4j2.xml \
+      -Dlogfile.name="monitor${i}.log" \
+      -jar target/monitoring-0.1.jar \
+      -node $i \
+      -monitoringinputs $INEQUALITY_INPUTS_PATH &
 done
