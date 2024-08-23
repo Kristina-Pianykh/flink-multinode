8873c5aa7ce19283568dcb1af9b9815c08802e41
Kristina Pianykh <kristinavrnrus@gmail.com>
2024-08-24 00:26:51 +0200

    chore: cleanup

--- deploying/example_inputs/count_matches.sh
+++
@@ -1,6 +0,0 @@
-for D in "$@"; do
- echo -n "PP $(cat "$D"/parallel*/parallel_example/0.log | grep LATE | wc -l) " 
- echo -n "SPP $(cat "$D"/stateparallel*/parallel_example/0.log | grep LATE | wc -l) "
- echo -n  "SPSP $(cat "$D"/stateparallel*/stateparallel_example/9.log | grep LATE | wc -l) "
- echo " ($D)"
-done                                                                            
--- deploying/example_inputs/get_duration.sh
+++
@@ -1,5 +0,0 @@
-for D in "$@"; do
- echo -n "PP $(cat "$D"/parallel*/parallel_example/trace*.csv | sort | tail -n1 | cut -d "," -f 1) " 
- echo -n "SP(S)P $(cat "$D"/stateparallel*/stateparallel_example/trace*.csv | sort | tail -n1 | cut -d "," -f 1) " 
- echo "($D)"
-done                                                                            
