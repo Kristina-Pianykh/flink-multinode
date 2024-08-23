bea274d5a652ece544c70e078108d9a661e202c4
Kristina Pianykh <kristinavrnrus@gmail.com>
2024-08-23 15:33:50 +0200

    refactor: test all dirs in topology path

--- rolling_test.sh
+++ rolling_test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-TOPOLOGY_BASE_DIR=/Users/krispian/Uni/bachelorarbeit/topologies_test
+TOPOLOGY_BASE_DIR=/Users/krispian/Uni/bachelorarbeit/topologies_delayed_inflation
 
 run() {
   ####################
@@ -13,7 +13,7 @@ run() {
 
   # start flink job
   cd /Users/krispian/Uni/bachelorarbeit/sigmod24-flink/deploying
-  ./run_local $INPUT_DIR $OUTPUT_DIR_NO_STRATEGY "${INPUT_DIR}/trace_inflated_1" $i
+  ./run_local $INPUT_DIR $OUTPUT_DIR_NO_STRATEGY $TRACE_DIR $NODE_N
   sleep 10
 
   ####################
@@ -31,11 +31,11 @@ run() {
       --add-opens java.base/java.lang=ALL-UNNAMED \
       -jar coordinator/target/coordinator-0.1.jar \
       -addressBook /Users/krispian/Uni/bachelorarbeit/sigmod24-flink/deploying/address_book_localhost.json \
-      -n $i &
+      -n $NODE_N &
 
   # start flink job
   cd /Users/krispian/Uni/bachelorarbeit/sigmod24-flink/deploying
-  ./run_local $INPUT_DIR $OUTPUT_DIR_STRATEGY "${INPUT_DIR}/trace_inflated_1" $i
+  ./run_local $INPUT_DIR $OUTPUT_DIR_STRATEGY $TRACE_DIR $NODE_N
   sleep 10
 
   # plot transmission rates
@@ -46,37 +46,16 @@ run() {
       --output_dir $INPUT_DIR --events "A;B;C;SEQ(A, B)"
 }
 
-
-for i in {5..9}
+for i in $(ls $TOPOLOGY_BASE_DIR)
 do
-  TOPOLOGY_PATH="${TOPOLOGY_BASE_DIR}/SEQ_ABC_${i}"
+  echo $i
+  NODE_N=$(echo $i | awk '{n=split($1,A,"_"); print A[n]}')
+  echo $NODE_N
+
+  TOPOLOGY_PATH="${TOPOLOGY_BASE_DIR}/${i}"
   INPUT_DIR="${TOPOLOGY_PATH}/plans"
+  TRACE_DIR="${INPUT_DIR}/trace_inflated_mix"
   OUTPUT_DIR_NO_STRATEGY="${INPUT_DIR}/output_strategy_0"
   OUTPUT_DIR_STRATEGY="${INPUT_DIR}/output_strategy_1"
-
-  echo "TOPOLOGY_PATH: $TOPOLOGY_PATH"
-  echo "INPUT_DIR: $INPUT_DIR"
-  echo "OUTPUT_DIR_NO_STRATEGY: $OUTPUT_DIR_NO_STRATEGY"
-  echo "OUTPUT_DIR_STRATEGY: $OUTPUT_DIR_STRATEGY"
   run
-
 done
-
-i=5
-TOPOLOGY_PATH="${TOPOLOGY_BASE_DIR}/AND_ABC_${i}"
-INPUT_DIR="${TOPOLOGY_PATH}/plans"
-OUTPUT_DIR_NO_STRATEGY="${INPUT_DIR}/output_strategy_0"
-OUTPUT_DIR_STRATEGY="${INPUT_DIR}/output_strategy_1"
-run
-
-
-TOPOLOGY_PATH="${TOPOLOGY_BASE_DIR}/SEQ_ABC_complex_${i}"
-INPUT_DIR="${TOPOLOGY_PATH}/plans"
-OUTPUT_DIR_NO_STRATEGY="${INPUT_DIR}/output_strategy_0"
-OUTPUT_DIR_STRATEGY="${INPUT_DIR}/output_strategy_1"
-
-echo "TOPOLOGY_PATH: $TOPOLOGY_PATH"
-echo "INPUT_DIR: $INPUT_DIR"
-echo "OUTPUT_DIR_NO_STRATEGY: $OUTPUT_DIR_NO_STRATEGY"
-echo "OUTPUT_DIR_STRATEGY: $OUTPUT_DIR_STRATEGY"
-run
