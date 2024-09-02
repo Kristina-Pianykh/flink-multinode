14db129e4bdaa7cc5d50e3f07ce9365fec38f03f
Kristina Pianykh <kristinavrnrus@gmail.com>
2024-09-02 12:05:41 +0200

    chore: add check if path is a dir

--- python/generate_mixed_traces.sh
+++ python/generate_mixed_traces.sh
@@ -3,14 +3,18 @@
 BASE_DIR=$(realpath "$(dirname "$0")")
 echo "BASE_DIR: $BASE_DIR"
 
-TOPOLOGY_DIR="/Users/krispian/Uni/bachelorarbeit/topologies_delayed_inflation"
+TOPOLOGY_DIR="/Users/krispian/Uni/bachelorarbeit/topologies_delayed_systematic_inflation"
 
 for i in $(ls $TOPOLOGY_DIR)
 do
-  poetry run python ${BASE_DIR}/mix_traces.py \
-      --topology_dir "${TOPOLOGY_DIR}/${i}"
-  if [ $? -ne 0 ]; then
-    echo "Error mixing traces for ${TOPOLOGY_DIR}/${i}"
-    exit 1
+  if [[ -d ${TOPOLOGY_DIR}/${i} ]]; then
+    poetry run python ${BASE_DIR}/mix_traces.py \
+        --topology_dir "${TOPOLOGY_DIR}/${i}"
+    if [ $? -ne 0 ]; then
+      echo "Error mixing traces for ${TOPOLOGY_DIR}/${i}"
+      exit 1
+    fi
+  else
+    echo "$i is not a directory"
   fi
 done
